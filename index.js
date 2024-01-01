6ee1f2ce1614d4c6a86af6a2470c943ad4e39e57
fevziyildirimm <fevziyildirmm@gmail.com>
2024-01-01 16:14:39 +0300

    schedule yapıya geçildi && /soru kanalı açıldı

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const schedule = require('node-schedule');
 const fs = require('fs');
 
 const bot = new TelegramBot(process.env.API_TOKEN, { polling: true });
@@ -9,25 +10,45 @@ function getRandomQuestion() {
   const randomIndex = Math.floor(Math.random() * questions.length);
   return questions[randomIndex];
 }
+const scheduledMessage = schedule.scheduleJob('*/30 * * * *', () => { //schedule her yarım saatte bir çalışacak sekilde ayarlandı.
+  for (let index = 0; index < 10; index++) {
+    sendQuestion();
+  }
+});
 
 function sendQuestion() {
   const chatId = process.env.CHAT_ID;
   const { question, options, answer } = getRandomQuestion();
-   const answers=[getRandomQuestion().options,getRandomQuestion().options];
-   const index=(answers.length+1) * Math.random();
-   answers.splice(index | 0, 0, options)
+  const answers = [getRandomQuestion().options, getRandomQuestion().options];
+  const index = (answers.length + 1) * Math.random();
+  answers.splice(index | 0, 0, options)
 
-   
 
-const pollOptions = {
-    is_anonymous: false, 
-    allows_multiple_answers: false, 
+
+  const pollOptions = {
+    is_anonymous: false,
+    allows_multiple_answers: false,
     correct_option_id: index,
-    type:"quiz"
+    type: "quiz"
   };
-  
-  
-  bot.sendPoll(chatId, question,answers,pollOptions);
+  bot.sendPoll(chatId, question, answers, pollOptions);
 }
 
-setInterval(sendQuestion, 18000000); 
+
+bot.onText(/\/soru/, (msg) => {
+  for (let index = 0; index < 10; index++) {
+    const { question, options, answer } = getRandomQuestion();
+    const answers = [getRandomQuestion().options, getRandomQuestion().options];
+    const index = (answers.length + 1) * Math.random();
+    answers.splice(index | 0, 0, options)
+    const pollOptions = {
+      is_anonymous: false,
+      allows_multiple_answers: false,
+      correct_option_id: index,
+      type: "quiz"
+    };
+
+
+    bot.sendPoll(msg.chat.id, question, answers, pollOptions);
+  }
+});
